e9861774b82e02e47204e891db4afabe085f21d3
Rokib Hasan Sagar <rokibhasansagar2014@outlook.com>
2020-04-17 00:46:54 +0600

    Crypto changes

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,11 +9,6 @@ TARGET_BOARD_PLATFORM := msm8996
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
 
-# Flags
-#TARGET_GLOBAL_CFLAGS +=
-#TARGET_GLOBAL_CPPFLAGS +=
-#COMMON_GLOBAL_CFLAGS +=
-
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
@@ -81,16 +76,19 @@ TW_USE_NEW_MINADBD := true
 
 # Encryption support
 TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
+TW_INCLUDE_CRYPTO_FBE := true
 # Disable Qualcomm and Samsung encryption as we are missing msm8996 keystore support
 #TW_INCLUDE_CRYPTO_SAMSUNG := true
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw
 
 # Asian region languages
 TW_EXTRA_LANGUAGES := true
 
 # Debug flags
-#TWRP_INCLUDE_LOGCAT := true
+TWRP_INCLUDE_LOGCAT := true
 #TARGET_USES_LOGD := true
 
 # Init properties from bootloader version, ex. model info
